5cedd995d6a52be36a3b541be0548434f7b469e7
sunho0248 <sunho0248@naver.com>
2026-08-09 17:56:07 +0900

    chore: prepare v1.3.6 update test release

--- src/WalkDistance.Update/ReleaseUpdate.cs
+++ src/WalkDistance.Update/ReleaseUpdate.cs
@@ -16,7 +16,7 @@ public static class GitHubReleaseClient
     public static HttpClient CreateClient()
     {
         var client = new HttpClient { Timeout = TimeSpan.FromSeconds(10) };
-        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("WalkDistance", "1.3.5"));
+        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("WalkDistance", "1.3.6"));
         client.DefaultRequestHeaders.Accept.ParseAdd("application/vnd.github+json");
         return client;
     }
